6a493bf86d794c94af9cd2289eb2115b4492242b
newgabo <gabrielcillo@gmail.com>
2021-11-29 17:30:41 -0300

    Create S01_prepara_dataset.r

---
+++ src/clustering/S01_prepara_dataset.r
@@ -0,0 +1,30 @@
+require("data.table")
+require("randomForest")
+
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+setwd( "~/buckets/b1/" )
+
+#leo el dataset , aqui se puede usar algun super dataset con Feature Engineering
+dataset  <- fread( "datasetsOri/paquete_premium.csv.gz", stringsAsFactors= TRUE)
+gc()
+
+#achico el dataset
+dataset[  ,  azar := runif( nrow(dataset) ) ]
+dataset  <-  dataset[  clase_ternaria =="BAJA+1"  & foto_mes>=202001  & foto_mes<=202011, ]
+gc()
+
+
+#quito los nulos para que se pueda ejecutar randomForest,  Dios que algoritmo prehistorico ...
+dataset  <- na.roughfix( dataset )
+gc()
+
+
+#Grabo el dataset
+fwrite( dataset,
+        paste0( "./datasets/dataset_clustering.csv.gz" ),
+        logical01 = TRUE,
+        sep= "," )
